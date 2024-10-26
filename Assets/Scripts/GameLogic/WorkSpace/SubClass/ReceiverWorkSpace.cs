bd18f70ed1940b4dbaa2278a8477bce4de7bfefb
ShunCena <shun.yosh.unityonly@gmail.com>
2024-10-26 10:34:51 +0900

    Teleporter & Receiver

---
+++ Assets/Scripts/GameLogic/WorkSpace/SubClass/ReceiverWorkSpace.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sync;
+using UI;
+
+namespace GameLogic.WorkSpace
+{
+    public class ReceiverWorkSpace : BaseWorkSpace
+    {
+        PlayerCustomPropertyCallback _playerCustomPropertyCallback;
+        [SerializeField] ReceiverTextView _receiverTextView;
+
+        public override void InitializeWorkSpace()
+        {
+            base.InitializeWorkSpace();
+            _playerCustomPropertyCallback = GetComponent<PlayerCustomPropertyCallback>();
+
+            var receiverPutAndTake = (ReceierPutAndTake)_putAndTake;
+
+            _putAndTake.OnPut.AddListener((item) => _grabbableVisualizer.Show(item));
+            _putAndTake.OnTake.AddListener(() => _grabbableVisualizer.Delete());
+
+            receiverPutAndTake.OnSenderIdSet.AddListener((index) => _receiverTextView.ShowText(index));
+
+            _playerCustomPropertyCallback.onComplete.AddListener(receiverPutAndTake.SetReceivedItem);
+        }
+    }
+}
---
+++ Assets/Scripts/GameLogic/WorkSpace/SubClass/TeleporterWorkSpace.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UI;
+
+namespace GameLogic.WorkSpace
+{
+    public class TeleporterWorkSpace : BaseWorkSpace
+    {
+        [SerializeField] TeleporterTextView _teleporterTextView;
+        public override void InitializeWorkSpace()
+        {
+            base.InitializeWorkSpace();
+            _putAndTake.OnPut.AddListener((item) => _putAndTake.Put(item));
+
+            var teleporterPutAndTake = (TeleporterPutAndTake)_putAndTake;
+            teleporterPutAndTake.OnReceiverIDSet.AddListener((playerNum) => _teleporterTextView.ShowText(playerNum));
+        }
+
+        private void Start()
+        {
+            InitializeWorkSpace();
+        }
+    }
+}
